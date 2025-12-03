c3ce9af478fb2f157579268444205eacba74ecba
sadit <medalitflores08@gmail.com>
2025-12-03 10:04:21 -0500

    tercer cambio 03/12/2025

--- index.php
+++ index.php
@@ -6,7 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Mi primer respositorio en GitHub</h1>
+    
+    <h1>Mi primer repositorio en GitHub</h1>
     <p>Este es el contenido de mi primer repositorio en GitHub.</p>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="about.php">Acerca de</a></li>
+            <li><a href="contact.php">Contacto</a></li>
+        </ul>
+    </nav>
+
 </body>
 </html>
